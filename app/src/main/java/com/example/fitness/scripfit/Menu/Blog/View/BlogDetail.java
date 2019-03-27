e9dd47c86cb3959f769e15c07c233a0f607f9f02
Daniel <daniel.pradana@technopartner.id>
2019-03-27 19:58:18 +0700

    update

---
+++ app/src/main/java/com/example/fitness/scripfit/Menu/Blog/View/BlogDetail.java
@@ -0,0 +1,15 @@
+package com.example.fitness.scripfit.Menu.Blog.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fitness.scripfit.R;
+
+public class BlogDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog_detail);
+    }
+}
